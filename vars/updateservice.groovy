0192f4798b2f40c97c3364af802d951c0c4164b0
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-06-30 16:38:12 +0530

    Update updateservice.groovy

--- vars/updateservice.groovy
+++ vars/updateservice.groovy
@@ -7,4 +7,17 @@ def call(String serviceName, Integer desiredCount = 1, String cluster = 'devclus
           --service ${serviceName} \
           --desired-count ${desiredCount}
     """
+    waitForServiceStability(serviceName, cluster)
+}
+
+def waitForServiceStability(String serviceName, String cluster) {
+    echo "Waiting for ECS service '${serviceName}' in cluster '${cluster}' to become stable..."
+
+    sh """
+        aws ecs wait services-stable \
+          --cluster ${cluster} \
+          --services ${serviceName}
+    """
+
+    echo "ECS service '${serviceName}' is now stable."
 }
