ae67ad3fbbb8f783abb5ce0cb467b4814e7687be
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-06-30 18:18:26 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,4 +1,4 @@
-def call(imageName, imageTag) {
+def call(String imageName, String imageTag) {
     sh """
         if docker images | grep -q ${imageName}; then
             docker rmi -f ${imageName}:${imageTag}
