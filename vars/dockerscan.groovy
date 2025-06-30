b5432d0dd2add6b9c005237a421b0bf98a86acbb
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-06-30 18:28:06 +0530

    Create dockerscan.groovy

---
+++ vars/dockerscan.groovy
@@ -0,0 +1,6 @@
+def scanDockerImage( imageFullName) {
+    sh "trivy image --format json -o scan_result.json ${imageFullName}"
+    def result = readJSON file: 'scan_result.json'
+    return result.Results.collectMany { it.Vulnerabilities ?: [] }
+                         .count { it.Severity in ['HIGH', 'CRITICAL'] }
+}
