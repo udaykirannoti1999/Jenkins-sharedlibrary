5f91d4b174583a58ce6bdf28d33468b99d0e2e16
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-07 17:05:53 +0530

    Update slackchannel.groovy

--- vars/slackchannel.groovy
+++ vars/slackchannel.groovy
@@ -1,1 +1,21 @@
+def call(String customMessage, String channel) {
+    def slackChannel = channel ?: '#default-channel'  // Use passed channel or fallback
+
+    wrap([$class: 'BuildUser']) {
+        sh 'git show -s --pretty=%an > commit.txt'
+        def author = readFile('commit.txt').trim()
+        def result = currentBuild.currentResult ?: 'SUCCESS'
+
+        slackSend(
+            color: COLOR_MAP[result],
+            channel: slackChannel,
+            message: "*${result}:* Job ${env.JOB_NAME}\n" +
+                     "Author Name: ${author}\n" +
+                     "Build user: ${BUILD_USER}\n" +
+                     "${customMessage}\n" +
+                     "More info at: ${env.BUILD_URL}"
+        )
+        cleanWs()
+    }
+}
 
