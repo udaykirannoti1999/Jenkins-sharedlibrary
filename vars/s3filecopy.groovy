08868ab6fc46796415240136584b3afd5b6f75ea
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-07 13:52:08 +0530

    Update s3filecopy.groovy

--- vars/s3filecopy.groovy
+++ vars/s3filecopy.groovy
@@ -1,1 +1,8 @@
 
+def call(String localFilePath, String s3DestinationPath) {
+    if (!localFilePath || !s3DestinationPath) {
+        error "Both localFilePath and s3DestinationPath are required."
+    }
+
+    sh "aws s3 cp ${localFilePath} ${s3DestinationPath}"
+}
