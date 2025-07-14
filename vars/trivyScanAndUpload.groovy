88520012d22c2bdeb056b430ee8db45417f951d6
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-14 16:47:52 +0530

    Update trivyScanAndUpload.groovy

--- vars/trivyScanAndUpload.groovy
+++ vars/trivyScanAndUpload.groovy
@@ -4,24 +4,27 @@ def call(Map config) {
     }
 
     def imageFullName = config.imageFullName
-    def reportName = "trivy-results.json"
+    def jsonReportName = "trivy-results.json"
+    def htmlReportName = "trivy-results.html"
 
     script {
         stage('Trivy Security Scan') {
             sh """
-        mkdir -p ${env.TRIVY_CACHE_DIR}
-        curl -sSL -o html.tpl ${env.TRIVY_TEMPLATE_URL}
-        trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format json -o ${env.TRIVY_JSON_REPORT} ${imageFullName}
-        trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format template --template "@html.tpl" -o ${env.TRIVY_HTML_REPORT} ${imageFullName}
-    """
-            def scanResults = readJSON file: reportName
+                mkdir -p ${env.TRIVY_CACHE_DIR}
+                curl -sSL -o html.tpl ${env.TRIVY_TEMPLATE_URL}
+                trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format json -o ${jsonReportName} ${imageFullName}
+                trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format template --template "@html.tpl" -o ${htmlReportName} ${imageFullName}
+            """
+
+            def scanResults = readJSON file: jsonReportName
             def criticalCount = 0
 
             scanResults?.Results?.each { result ->
                 criticalCount += result.Vulnerabilities?.count { it.Severity == 'CRITICAL' } ?: 0
             }
 
-            uploadReportToS3(reportName, config)
+            uploadReportToS3(jsonReportName, config)
+            uploadReportToS3(htmlReportName, config)
 
             if (criticalCount > 0 && config.get('failBuild', true)) {
                 error "BUILD FAILED: ${criticalCount} CRITICAL vulnerabilities found."
@@ -35,9 +38,10 @@ def call(Map config) {
 }
 
 private def uploadReportToS3(String reportName, Map config) {
-    def s3Key = "trivy-scans/${config.imageFullName.replace(':', '/')}-report.json"
+    def reportType = reportName.endsWith(".html") ? "html" : "json"
+    def s3Key = "trivy-scans/${config.imageFullName.replace(':', '/')}-report.${reportType}"
     def s3Uri = "s3://${config.s3Bucket}/${s3Key}"
 
     sh "aws s3 cp ${reportName} ${s3Uri}"
     echo "Report uploaded to ${s3Uri}"
-} 
+}
