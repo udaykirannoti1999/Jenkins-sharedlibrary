ce59a0f5a671a7acd42ea00cd8d6ae136fe8d64e
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-14 16:12:47 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,1 +1,17 @@
+// vars/dockerPush.groovy
+def call(String fullImageName) {
+    if (!fullImageName) {
+        error "❌ dockerPush: fullImageName is required."
+    }
+
+    try {
+        def ecrDomain = fullImageName.split('/')[0]
+        sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${ecrDomain}"
+        sh "docker push ${fullImageName}"
+
+        echo "✅ Docker image pushed: ${fullImageName}"
+    } catch (err) {
+        error "❌ dockerPush failed: ${err.getMessage()}"
+    }
+}
 
