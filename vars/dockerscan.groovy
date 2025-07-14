ad2a4898a9b2898eb1cddbd98ce00588a4b8df84
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-14 16:41:54 +0530

    Update dockerscan.groovy

--- vars/dockerscan.groovy
+++ vars/dockerscan.groovy
@@ -1,4 +1,13 @@
 def call(String imageFullName) {
-    sh "trivy image --format json -o scan_result.json ${imageFullName}"
-    def result = readJSON file: 'scan_result.json'
+        // Generate JSON + HTML
+    sh """
+        mkdir -p ${env.TRIVY_CACHE_DIR}
+        curl -sSL -o html.tpl ${env.TRIVY_TEMPLATE_URL}
+        trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format json -o ${env.TRIVY_JSON_REPORT} ${imageFullName}
+        trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format template --template "@html.tpl" -o ${env.TRIVY_HTML_REPORT} ${imageFullName}
+    """
+
+    def result = readJSON file: env.TRIVY_JSON_REPORT
+    return result.Results.collectMany { it.Vulnerabilities ?: [] }
+                         .count { it.Severity in ['HIGH', 'CRITICAL'] }
 }
