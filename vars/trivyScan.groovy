3584ccb1244e63bbdbef2ba324b33b46f6e2e477
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-19 19:30:03 +0530

    Update trivyScan.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -3,13 +3,9 @@ def call(String buildGitBranch, String envTag) {
 
     echo "Scanning image: ${imageFullName}"
 
-    env.TRIVY_CACHE_DIR = "${env.WORKSPACE}/.trivycache"
-    env.TRIVY_HTML_REPORT = "${env.WORKSPACE}/trivy-report.html"
-    env.TRIVY_TEMPLATE_URL = "https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl"
-
     sh """
-        mkdir -p ${env.TRIVY_CACHE_DIR}
-        curl -sSL -o html.tpl ${env.TRIVY_TEMPLATE_URL}
-        trivy image --cache-dir ${env.TRIVY_CACHE_DIR} --format template --template "@html.tpl" -o ${env.TRIVY_HTML_REPORT} ${imageFullName}
+        # Run Trivy and show only HIGH and CRITICAL vulnerabilities in table format
+        trivy image --severity HIGH,CRITICAL ${imageFullName}
     """
-}
+} 
+
