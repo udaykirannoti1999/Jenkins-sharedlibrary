0abcee266d37580315c77e221b91e24763313e20
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-07-19 20:16:51 +0530

    Update trivyScan.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -6,13 +6,13 @@ def call(String buildGitBranch, String envTag) {
 
     // Run Trivy scan and write output to a file
     sh """
-        timestamp=        timestamp=\u$(date '+%Y-%m-%d %H:%M:%S')
+        timestamp=\$(date '+%Y-%m-%d %H:%M:%S')
         git_branch='${buildGitBranch}'
 
         trivy image --severity HIGH,CRITICAL --ignore-unfixed --format table ${imageFullName} > trivy-output.txt
 
         echo "<html><head><title>Trivy Report</title></head><body><h2>Trivy Scan Result</h2>" > ${reportFileHtml}
-        echo "<p>Scan Time: ${timestamp}</p><p>Git Branch: ${git_branch}</p><pre>" >> ${reportFileHtml}
+        echo "<p>Scan Time: \${timestamp}</p><p>Git Branch: \${git_branch}</p><pre>" >> ${reportFileHtml}
         cat trivy-output.txt >> ${reportFileHtml}
         echo "</pre></body></html>" >> ${reportFileHtml}
     """
@@ -26,4 +26,5 @@ def call(String buildGitBranch, String envTag) {
         reportFiles: reportFileHtml,
         reportName: "Trivy Vulnerability Report"
     ]
-} 
+}
+
